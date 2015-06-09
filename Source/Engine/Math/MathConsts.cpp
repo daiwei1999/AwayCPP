50b06af1375427ee382666077eb0b7d50712f224
daiwei <daiwei_1999_82@163.com>
2015-06-09 23:03:32 +0800

    define MathConsts as float literal

--- Source/Engine/Math/MathConsts.cpp
+++ Source/Engine/Math/MathConsts.cpp
@@ -2,9 +2,9 @@
 
 USING_AWAY_NAMESPACE
 
-const float MathConsts::PI = 3.14159265358979323846264338327950288f;
-const float MathConsts::TWO_PI = PI * 2;
-const float MathConsts::HALF_PI = PI * .5f;
-const float MathConsts::RADIANS_TO_DEGREES = 180 / MathConsts::PI;
-const float MathConsts::DEGREES_TO_RADIANS = MathConsts::PI / 180;
+const float MathConsts::PI = 3.14159265f;
+const float MathConsts::TWO_PI = 6.28318531f;
+const float MathConsts::HALF_PI = 1.57079633f;
+const float MathConsts::RADIANS_TO_DEGREES = 57.2957795f;
+const float MathConsts::DEGREES_TO_RADIANS = 0.0174532925f;
 const float MathConsts::Infinity = std::numeric_limits<float>::infinity();
